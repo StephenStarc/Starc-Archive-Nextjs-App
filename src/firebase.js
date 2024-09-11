0b18faa0df216260bb7fd32ae29590a6e5638b7b
Stephen Starc <stephenstarc01@gmail.com>
2024-09-11 23:58:39 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,7 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD_XqAQe1I22fCDPo1X25lGO0kjH-agvvQ",
-    authDomain: "starc-archive.firebaseapp.com",
-    projectId: "starc-archive",
-    storageBucket: "starc-archive.appspot.com",
-    messagingSenderId: "34098903204",
-    appId: "1:34098903204:web:394786d0339301af018e31",
-    measurementId: "G-9S9ME4E8VW"
+    Add you config 
   };
 
 const app = initializeApp(firebaseConfig);
